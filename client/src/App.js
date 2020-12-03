af6c71d30f6308f5031eb0534b74bae1311f08ca
Mohamed.Yehia <mohamed.yehia>
2020-12-03 18:54:13 +0200

    Adding React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "reactstrap";
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello React</h1>
+        <Button color="danger">Danger!</Button>
+    </div>
+  );
+}
+
+export default App;
